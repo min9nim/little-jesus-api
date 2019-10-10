e26660ed7fcaa9bcf8b402c41231853834ed72ae
min9nim <min9nim@gmail.com>
2019-10-10 21:31:18 +0900

    Teacher 타입에 students 필드리졸버 추가

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,7 +1,14 @@
 import Query from './query'
 import Mutation from './mutation'
+import {models} from 'mongoose'
 
 export default {
   Query,
   Mutation,
+  Teacher: {
+    students: async ({_id}) => {
+      const teacher = await models.Teachers.findOne({_id}).populate('students').exec()
+      return teacher.students
+    }
+  }
 }
--- src/graphql/resolvers/query.ts
+++ src/graphql/resolvers/query.ts
@@ -1,12 +1,12 @@
 import {models} from 'mongoose'
 
 export default {
-  async students(){
+  async students() {
     const students = await models.Students.find({})
     return students
   },
-  async teachers(){
-    const teachers = await models.Teachers.find({}).populate({path: 'students', model: 'Students'}).exec()
+  async teachers() {
+    const teachers = await models.Teachers.find({})
     return teachers
-  }
+  },
 }
