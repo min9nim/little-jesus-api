bdf3114df5fba64ab00ad2bc7a01ced53dfc53e6
min9nim <min9nim@gmail.com>
2019-11-03 21:04:47 +0900

    deploy test

--- src/db/start-db.ts
+++ src/db/start-db.ts
@@ -1,20 +1,22 @@
 import mongoose from 'mongoose'
 import registerSchema from './mongoose-schema'
 
-
-export default function startDB(){
+export default function startDB() {
   const {NOW_GITHUB_COMMIT_REF, dburl_dev, dburl} = process.env
 
   const database_url = NOW_GITHUB_COMMIT_REF === 'master' ? dburl : dburl_dev
-  if(!database_url){
+  if (!database_url) {
     throw Error('database_url is not defined')
   }
   registerSchema()
-  mongoose.connect(database_url, {useNewUrlParser: true});
+  mongoose.connect(
+    database_url,
+    {useNewUrlParser: true}
+  )
 }
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'DB connection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'DB connection error:'))
 db.once('open', function() {
   console.log('mongoDB connected successfully')
-});
+})
