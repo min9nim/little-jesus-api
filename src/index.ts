656191b9fdb21c3c861276adb6028058a1d056ee
min9nim <min9nim@gmail.com>
2019-12-21 01:24:22 +0900

    add tracing option to ApolloServer

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ import typeDefs from './graphql/type-defs'
 
 startDB()
 
-const server = new ApolloServer({typeDefs, resolvers, introspection: true, playground: true})
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+  tracing: true,
+})
 const port = process.env.PORT || 5050
 server.listen({port}).then(({url}) => {
   console.log(`🚀  Server ready at ${url}`)
