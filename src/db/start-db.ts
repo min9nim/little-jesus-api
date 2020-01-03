80aa094debe048532254c70f79474c60399afb19
min9nim <min9nim@gmail.com>
2020-01-03 15:54:57 +0900

    update

--- src/db/start-db.ts
+++ src/db/start-db.ts
@@ -12,7 +12,6 @@ export default function startDB() {
     database_url = dburl_2020
   }
   console.log('database_url = ' + database_url)
-  database_url = 'mongodb://admin:admin123@ds259518.mlab.com:59518/little-jesus-2020'
   if (!database_url) {
     throw Error('database_url is not defined')
   }
