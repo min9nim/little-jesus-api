632e6f400c3a33d60e16eca5ae22136a8ded039c
min9nim <min9nim@gmail.com>
2020-02-23 19:38:56 +0900

    update log

--- src/db/start-db.ts
+++ src/db/start-db.ts
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose'
 import registerSchema from './mongoose-schema'
+import createLogger from 'if-logger'
+
+const logger = createLogger().addTags('start-db.ts')
 
 export default function startDB() {
   const {NOW_GITHUB_COMMIT_REF, dburl_dev, dburl, dburl_2020} = process.env
   let database_url = dburl_dev
-  console.log('NOW_GITHUB_COMMIT_REF = ' + NOW_GITHUB_COMMIT_REF)
+  logger.info('NOW_GITHUB_COMMIT_REF = ' + NOW_GITHUB_COMMIT_REF)
   if (NOW_GITHUB_COMMIT_REF === 'lj2019') {
     database_url = dburl
   }
   if (NOW_GITHUB_COMMIT_REF === 'lj2020' || NOW_GITHUB_COMMIT_REF === 'master') {
     database_url = dburl_2020
   }
-  console.log('database_url = ' + database_url)
+  logger.info('database_url = ' + database_url)
   if (!database_url) {
     throw Error('database_url is not defined')
   }
@@ -26,9 +29,9 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'DB connection error:'))
 db.once('open', function() {
   if (process.env.NOW_GITHUB_COMMIT_REF !== 'develop') {
-    console.log('dev mongoDB connected successfully')
+    logger.verbose('dev mongoDB connected successfully')
   } else {
-    console.log(
+    logger.info(
       '\x1b[33m%s\x1b[0m',
       'PRODUCTION-' + process.env.NOW_GITHUB_COMMIT_REF,
       'mongoDB connected successfully'
