c51d8db83fc141a4d84480c80e853473df65cf88
min9nim <min9nim@gmail.com>
2020-02-23 19:24:24 +0900

    master브랜치가 2020디비를 바라보도록 세팅

--- src/db/start-db.ts
+++ src/db/start-db.ts
@@ -4,13 +4,13 @@ import registerSchema from './mongoose-schema'
 export default function startDB() {
   const {NOW_GITHUB_COMMIT_REF, dburl_dev, dburl, dburl_2020} = process.env
   let database_url = dburl_dev
-  if(NOW_GITHUB_COMMIT_REF === 'master'){
+  console.log('NOW_GITHUB_COMMIT_REF = ' + NOW_GITHUB_COMMIT_REF)
+  if (NOW_GITHUB_COMMIT_REF === 'lj2019') {
     database_url = dburl
   }
-  if(NOW_GITHUB_COMMIT_REF === 'lj2020'){
+  if (NOW_GITHUB_COMMIT_REF === 'lj2020' || NOW_GITHUB_COMMIT_REF === 'master') {
     database_url = dburl_2020
   }
-  console.log('NOW_GITHUB_COMMIT_REF = ' + NOW_GITHUB_COMMIT_REF)
   console.log('database_url = ' + database_url)
   if (!database_url) {
     throw Error('database_url is not defined')
@@ -25,9 +25,13 @@ export default function startDB() {
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'DB connection error:'))
 db.once('open', function() {
-  if(process.env.NOW_GITHUB_COMMIT_REF !== 'develop'){
+  if (process.env.NOW_GITHUB_COMMIT_REF !== 'develop') {
     console.log('dev mongoDB connected successfully')
-  }else{
-    console.log('\x1b[33m%s\x1b[0m', 'PRODUCTION-' + process.env.NOW_GITHUB_COMMIT_REF, 'mongoDB connected successfully')
+  } else {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      'PRODUCTION-' + process.env.NOW_GITHUB_COMMIT_REF,
+      'mongoDB connected successfully'
+    )
   }
 })
