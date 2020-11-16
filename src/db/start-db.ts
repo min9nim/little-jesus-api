402c2480001fede0f8e850f4e75d64ae7603a877
min9nim <min9nim@gmail.com>
2020-11-16 09:50:22 +0900

    add log

--- src/db/start-db.ts
+++ src/db/start-db.ts
@@ -6,6 +6,8 @@ const logger = createLogger().addTags('start-db.ts')
 
 export default function startDB() {
   const {NOW_GITHUB_COMMIT_REF, dburl_dev, dburl, dburl_2020} = process.env
+  logger.debug('xxx', {NOW_GITHUB_COMMIT_REF, dburl_dev, dburl, dburl_2020})
+
   let database_url = dburl_dev
   logger.info('NOW_GITHUB_COMMIT_REF = ' + NOW_GITHUB_COMMIT_REF)
   if (NOW_GITHUB_COMMIT_REF === 'lj2019') {
